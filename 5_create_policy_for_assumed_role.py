80168ef91f13e590a381763c85ca001cc129a06d
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-03-18 06:43:39 +0800

    added checks if policy for assumed role already exists

--- 5_create_policy_for_assumed_role.py
+++ 5_create_policy_for_assumed_role.py
@@ -4,20 +4,30 @@ import sys
 
 policy_name_for_assumed_role = sys.argv[1]
 
-
 def create_iam_policy(policy_name, policy_document):
-
     iam_client = boto3.client("iam")
 
+    # Check if the policy already exists
+    try:
+        paginator = iam_client.get_paginator('list_policies')
+        for response in paginator.paginate(Scope='Local'):
+            for policy in response['Policies']:
+                if policy['PolicyName'] == policy_name:
+                    print(f"Policy {policy_name} already exists.")
+                    return policy['Arn']
+    except Exception as e:
+        print("Failed to list IAM policies:", str(e))
+        exit(1)
+
+    # Create the policy if it does not exist
     try:
         response = iam_client.create_policy(
             PolicyName=policy_name, PolicyDocument=policy_document
         )
-        return response["Policy"]["Arn"]
+        return response['Policy']['Arn']
     except Exception as e:
         print("Failed to create IAM policy:", str(e))
         exit(1)
-
 
 policy_document = {
     "Version": "2012-10-17",
@@ -36,7 +46,7 @@ policy_document = {
     ],
 }
 
-# Create IAM policy
+# Create or retrieve existing IAM policy
 policy_arn = create_iam_policy(
     policy_name_for_assumed_role, json.dumps(policy_document)
 )
